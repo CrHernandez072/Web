dd19d17fd05661f82fb33238a45997461349afd7
cristian <isc.cristian.hernandez@gmail.com>
2018-03-10 22:58:26 -0800

    Sirve y consume Json, para consumir se utilizan las clases models.py, views.py y urls.py. para consumir se usa models.py urls.py y se crean archivos html en los templates

--- Django/urls.py
+++ Django/urls.py
@@ -20,4 +20,23 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('base/', views.view_base),
+
+	#6 MARZO Servir Json
+    path("json_autor/", views.json_autor),
+    path("json_editorial/", views.json_editorial),
+    path("json_libro/", views.json_libro),
+    path("json_detalle_libro/", views.json_detalle_libro),
+    path("json_disponibilidad_libro/", views.json_disponibilidad_libro),
+    path("json_modelo_extendido/", views.json_modelo_extendido),
+
+	#6 MARZO Consumir Json
+    path("consumir_autor/", views.consumir_autor),
+    path("consumir_detalle/", views.consumir_detalle),
+    path("consumir_disponibilidad/", views.consumir_disponibilidad),
+    path("consumir_editorial/", views.consumir_editorial),
+    path("consumir_libro/", views.consumir_libro),
+    path("consumir_extendido/", views.consumir_extendido),
+
+#-------- FIN 6 DE MARZO---------
+
 ]
